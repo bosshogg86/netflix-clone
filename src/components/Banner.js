99b129cc6f16126331f27a011bf3bcd2c82c97ec
bosshogg86 <jeffhogg86@gmail.com>
2020-11-19 21:27:42 -0800

    adds react-youtube, movie-trailer, deploys

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -42,6 +42,7 @@ const Banner = () => {
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
+      <div className="banner--fadeBottom" />
     </header>
   );
 };
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -16,6 +19,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playersVars: {
+      //https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = movie => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -23,6 +48,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map(movie => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -31,7 +57,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      {/* container > posters*/}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
